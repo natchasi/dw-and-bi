8fc0ac24175ede5716c8d1b214d6a9ad6e29a37f
natchasi <157198683+natchasi@users.noreply.github.com>
2024-02-19 15:21:12 +0000

    Edit host redshif cluster

--- 03-building-a-data-warehouse/etl.py
+++ 03-building-a-data-warehouse/etl.py
@@ -69,10 +69,10 @@ def insert_tables(cur, conn):
 
 
 def main():
-    host = "redshift-cluster-1.cwryxvpilhye.us-east-1.redshift.amazonaws.com"
+    host = "redshift-cluster-1.c9hyvw5myiqr.us-east-1.redshift.amazonaws.com"
     dbname = "dev"
     user = "awsuser"
-    password = ""
+    password = "Kiddieme88"
     port = "5439"
     conn_str = f"host={host} dbname={dbname} user={user} password={password} port={port}"
     conn = psycopg2.connect(conn_str)
